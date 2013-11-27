cb3a4b4fd748efb224ce58a3f0ed3dfe1e152bde
Nick McCready <nmccready@gmail.com>
2013-11-27 08:39:06 -0500

    heat layer example start

--- example/example-controller.js
+++ example/example-controller.js
@@ -1,50 +1,53 @@
-
 (function () {
     var module = angular.module("angular-google-maps-example", ["google-maps"]);
 }());
 
-function ExampleController ($scope, $timeout, $log) {
+function ExampleController($scope, $timeout, $log) {
 
     // Enable the new Google Maps visuals until it gets enabled by default.
     // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
     google.maps.visualRefresh = true;
 
-    onMarkerClicked = function(marker){
+    onMarkerClicked = function (marker) {
         marker.showWindow = true;
-        window.alert("Marker: lat: " + marker.latitude +", lon: " + marker.longitude + " clicked!!")
+        window.alert("Marker: lat: " + marker.latitude + ", lon: " + marker.longitude + " clicked!!")
     };
 
-    genRandomMarkers = function(numberOfMarkers,scope){
+    genRandomMarkers = function (numberOfMarkers, scope) {
         markers = []
-        for(var i=0; i < numberOfMarkers; i++){
+        for (var i = 0; i < numberOfMarkers; i++) {
             markers.push(createRandomMarker(scope.map.bounds))
         }
         scope.map.randomMarkers = markers;
     };
-    
-    createRandomMarker = function(bounds) {
-      var lat_min = bounds.southwest.latitude,
-          lat_range = bounds.northeast.latitude - lat_min,
-          lng_min = bounds.southwest.longitude,
-          lng_range = bounds.northeast.longitude - lng_min;
+
+    createRandomMarker = function (bounds) {
+        var lat_min = bounds.southwest.latitude,
+                lat_range = bounds.northeast.latitude - lat_min,
+                lng_min = bounds.southwest.longitude,
+                lng_range = bounds.northeast.longitude - lng_min;
 
         latitude = lat_min + (Math.random() * lat_range);
         longitude = lng_min + (Math.random() * lng_range);
-        return {latitude:latitude,longitude:longitude};
+        return {latitude: latitude, longitude: longitude};
     };
-
     angular.extend($scope, {
         map: {
+            heatLayerCallback: function (layer) {
+                //set the heat layers backend data
+                var mockHeatLayer = new MockHeatLayer(layer);
+            },
             showTraffic: true,
             showBicycling: false,
             showWeather: false,
+            showHeat: false,
             center: {
                 latitude: 45,
                 longitude: -73
             },
             options: {
-              streetViewControl: false,
-              panControl: false
+                streetViewControl: false,
+                panControl: false
             },
             zoom: 3,
             dragging: false,
@@ -92,8 +95,8 @@ function ExampleController ($scope, $timeout, $log) {
             randomMarkers: [],
             doClusterRandomMarkers: true,
             doUgly: true, //great name :)
-            clusterOptions:{title:'Hi I am a Cluster!', gridSize:60, ignoreHidden:true,minimumClusterSize:2,
-                imageExtension:'png',imagePath:'http://localhost:3000/cluster',imageSizes:[72]},
+            clusterOptions: {title: 'Hi I am a Cluster!', gridSize: 60, ignoreHidden: true, minimumClusterSize: 2,
+                imageExtension: 'png', imagePath: 'http://localhost:3000/cluster', imageSizes: [72]},
             clickedMarker: {
                 title: 'You clicked here',
                 latitude: null,
@@ -139,34 +142,35 @@ function ExampleController ($scope, $timeout, $log) {
                     message: 'passed in from the opener'
                 }
             },
-            polylines: [{
-                path: [
-                    {
-                        latitude: 45,
-                        longitude: -74
+            polylines: [
+                {
+                    path: [
+                        {
+                            latitude: 45,
+                            longitude: -74
+                        },
+                        {
+                            latitude: 30,
+                            longitude: -89
+                        },
+                        {
+                            latitude: 37,
+                            longitude: -122
+                        },
+                        {
+                            latitude: 60,
+                            longitude: -95
+                        }
+                    ],
+                    stroke: {
+                        color: '#6060FB',
+                        weight: 3
                     },
-                    {
-                        latitude: 30,
-                        longitude: -89
-                    },
-                    {
-                        latitude: 37,
-                        longitude: -122
-                    },
-                    {
-                        latitude: 60,
-                        longitude: -95
-                    }
-                ],
-                stroke: {
-                    color: '#6060FB',
-                    weight: 3
+                    editable: true,
+                    draggable: false,
+                    geodesic: false,
+                    visible: true
                 },
-                editable:true,
-                draggable:false,
-                geodesic:false,
-                visible:true
-               },
                 {
                     path: [
                         {
@@ -190,35 +194,35 @@ function ExampleController ($scope, $timeout, $log) {
                         color: '#6060FB',
                         weight: 3
                     },
-                    editable:true,
-                    draggable:true,
-                    geodesic:true,
-                    visible:true
+                    editable: true,
+                    draggable: true,
+                    geodesic: true,
+                    visible: true
                 }
             ]
         },
-        toggleColor:function(color){
+        toggleColor: function (color) {
             return color == 'red' ? '#6060FB' : 'red';
         }
 
     });
 
-    _.each($scope.map.markers,function(marker){
-        marker.closeClick = function(){
+    _.each($scope.map.markers, function (marker) {
+        marker.closeClick = function () {
             marker.showWindow = false;
             $scope.$apply();
         };
-        marker.onClicked = function(){
+        marker.onClicked = function () {
             onMarkerClicked(marker);
         };
     });
 
-    _.each($scope.map.markers2,function(marker){
-        marker.closeClick = function(){
+    _.each($scope.map.markers2, function (marker) {
+        marker.closeClick = function () {
             marker.showWindow = false;
             $scope.$apply();
         };
-        marker.onClicked = function(){
+        marker.onClicked = function () {
             onMarkerClicked(marker);
         };
     });
@@ -238,25 +242,25 @@ function ExampleController ($scope, $timeout, $log) {
     };
     $scope.map.clusterOptionsText = JSON.stringify($scope.map.clusterOptions);
     $scope.$watch('map.clusterOptionsText', function (newValue, oldValue) {
-        if(newValue !== oldValue)
-            $scope.map.clusterOptions = angular.fromJson($scope.map.clusterOptionsText);    
+        if (newValue !== oldValue)
+            $scope.map.clusterOptions = angular.fromJson($scope.map.clusterOptionsText);
     });
 
-     $scope.$watch('map.doUgly', function (newValue, oldValue) {
+    $scope.$watch('map.doUgly', function (newValue, oldValue) {
         var json;
-        if(newValue !== oldValue){
+        if (newValue !== oldValue) {
             if (newValue)
-                json = {title:'Hi I am a Cluster!', gridSize:60, ignoreHidden:true,minimumClusterSize:2,
-                    imageExtension:'png',imagePath:'http://localhost:3000/cluster',imageSizes:[72]};
+                json = {title: 'Hi I am a Cluster!', gridSize: 60, ignoreHidden: true, minimumClusterSize: 2,
+                    imageExtension: 'png', imagePath: 'http://localhost:3000/cluster', imageSizes: [72]};
             else
-                json = {title:'Hi I am a Cluster!', gridSize:60, ignoreHidden:true,minimumClusterSize:2};
+                json = {title: 'Hi I am a Cluster!', gridSize: 60, ignoreHidden: true, minimumClusterSize: 2};
             $scope.map.clusterOptions = json;
             $scope.map.clusterOptionsText = angular.toJson(json);
         }
     });
 
-    $scope.genRandomMarkers = function(numberOfMarkers) {
-        genRandomMarkers(numberOfMarkers,$scope);
+    $scope.genRandomMarkers = function (numberOfMarkers) {
+        genRandomMarkers(numberOfMarkers, $scope);
     };
 
     $scope.searchLocation = {
@@ -272,29 +276,29 @@ function ExampleController ($scope, $timeout, $log) {
         // };
         $scope.map.infoWindow.show = true;
         dynamicMarkers = [
-                {
-                    latitude: 46,
-                    longitude: -79,
-                    showWindow: false
-                },
-                {
-                    latitude: 33,
-                    longitude: -79,
-                    showWindow: false
-                },
-                {
-                    icon: 'plane.png',
-                    latitude: 35,
-                    longitude: -127,
-                    showWindow: false
-                }
+            {
+                latitude: 46,
+                longitude: -79,
+                showWindow: false
+            },
+            {
+                latitude: 33,
+                longitude: -79,
+                showWindow: false
+            },
+            {
+                icon: 'plane.png',
+                latitude: 35,
+                longitude: -127,
+                showWindow: false
+            }
         ];
-       _.each(dynamicMarkers,function(marker){
-            marker.closeClick = function(){
+        _.each(dynamicMarkers, function (marker) {
+            marker.closeClick = function () {
                 marker.showWindow = false;
                 $scope.$apply();
             };
-            marker.onClicked = function(){
+            marker.onClicked = function () {
                 onMarkerClicked(marker);
             };
         });
